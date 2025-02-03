96edae3994a11d96df62891f1d40532856f5c6c3
Camillak2 <ksagdieva2@gmail.com>
2025-02-03 15:03:24 +0300

    Добавьте файлы проекта.

---
+++ StrayDogs/DB/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrayDogs.DB
+{
+    internal class DBConnection
+    {
+        public static Stray_DogsEntities stray_Dogs = new Stray_DogsEntities();
+
+        public static Employee logginedEmployee;
+    }
+}
+ 
